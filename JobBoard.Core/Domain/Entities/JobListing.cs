78e6618a9c84a181c7caee54c7baa61dfee03f93
jamestelcide <james.telcide@gmail.com>
2024-10-25 13:50:08 -0400

    Created JobListing Entity

---
+++ JobBoard.Core/Domain/Entities/JobListing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Core.Domain.Entities
+{
+    public class JobListing
+    {
+        [Key]
+        public Guid JobID { get; set; }
+        public string JobTitle { get; set; } = string.Empty;
+        public string CompnayName { get; set; } = string.Empty;
+        public string CityAndState { get; set; } = string.Empty;
+        public string PayRange { get; set; } = string.Empty;
+        public string JobType { get; set; } = string.Empty;
+        public string FullDescription { get; set; } = string.Empty;
+
+    }
+}
